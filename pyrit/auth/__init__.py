98f224e7c0cfad9ad1d44f6d8f2418c4ed5649c5
Satanisreal666 <Martinpaypal666@gmail.com>
2025-07-09 11:10:45 +0200

    Ctrl+enter

--- pyrit/auth/__init__.py
+++ pyrit/auth/__init__.py
@@ -9,4 +9,5 @@ from pyrit.auth.authenticator import Authenticator
 from pyrit.auth.azure_auth import AzureAuth
 from pyrit.auth.azure_storage_auth import AzureStorageAuth
 
+
 __all__ = ["Authenticator", "AzureAuth", "AzureStorageAuth"]
